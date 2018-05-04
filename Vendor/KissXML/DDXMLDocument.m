22ff0f8adcd9b22c01ed7080ff4679e4973f6155
Jindřich Sarson <jindra@tappytaps.com>
2018-05-04 11:38:34 +0200

    fixed libxml import

--- Vendor/KissXML/DDXMLDocument.m
+++ Vendor/KissXML/DDXMLDocument.m
@@ -1,6 +1,6 @@
 #import "DDXMLPrivate.h"
 #import "NSString+DDXML.h"
-
+@import libxml2;
 #if ! __has_feature(objc_arc)
 #warning This file must be compiled with ARC. Use -fobjc-arc flag (or convert project to ARC).
 #endif
